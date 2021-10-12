a5564d947ccd97d73642d1533aea4b9e9a8ee17c
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-10-13 01:57:03 +0300

    finishfirst

--- basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
+++ basiclibrary/lib/src/test/java/basiclibrary/LibraryTest.java
@@ -4,11 +4,43 @@
 package basiclibrary;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
         assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    }
+    @Test void testroll() {
+        Library rolltest = new Library();
+        ArrayList<Integer> correctresulte= rolltest.roll(6);
+        assertEquals(6, correctresulte.size());
+    }
+    @Test void testduplicates() {
+        Library duplicatestest = new Library();
+        int[] array = {1, 3, 5, 7, 4};
+        boolean correctresulte= duplicatestest. duplicates(array);
+        assertEquals(false, correctresulte);
+    }
+    @Test void testaverages() {
+        Library averagestest = new Library();
+        int[] avrgarray = {1, 3, 5, 7, 4};
+        double correctresulte= averagestest.averages(avrgarray);
+        assertEquals(4.0, correctresulte);
+    }
+    @Test void testavarageoftwoarray() {
+        Library avarageoftwoarraytest = new Library();
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        int[] correctresulte= avarageoftwoarraytest.avarageoftwoarray(weeklyMonthTemperatures);
+        int[] expect={55, 54, 60, 53, 59, 57, 61};
+        assertArrayEquals(expect, correctresulte);
     }
 }
